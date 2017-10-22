dbc36a1c1ce0564c7652fb212d27410988f2ea28
GrantRotzien <grantqrotzien@gmail.com>
2017-10-22 01:12:01 -0700

    add js form code, change error in css id assignments

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,43 @@
-$(document).ready(function) {
-  $("form#language").submit(function(event) {
-    var questionA = parseInt($("input#a").val());
-    var questionB = parseInt($("input#c").val());
-    var questionC = parseInt($("input#c").val());
-    var questionD = parseInt($("input#d").val());
-    var questionE = parseInt($("input#e").val());
-    var totalQuestionVal = addVal(questionA, questionB, questionC, questionD, questionE);
-    if (totalQuestionVal >=5 && totalQuestionVal <=7) {
-      $("javaResult").hide();
+$(document).ready(function() {
+  var addVal = function (a, b, c, d, e) {
+    return a + b + c + d + e;
+  };
+  $('form#findtrack').submit(function(event) {
+    event.preventDefault();
+    var a = parseInt($("select#jobtitleInput").val());
+    var b = parseInt($("select#mobileorwebInput").val());
+    var c = parseInt($("select#startupInput").val());
+    var d = parseInt($("select#csharporphpInput").val());
+    var e = parseInt($("select#jokeInput").val());
+    var totalVal = addVal(a, b, c, d, e);
+    if (totalVal >= 1 && totalVal <= 4) {
+      $("javaResult").show();
+      $("rubyResult").hide();
       $("phpResult").hide();
-      $("rubyResult").show();
-
-    } else if
-
-
-
+      $("csharpResult").hide();
     }
-  }
+    else if (totalVal >= 5 && totalVal <= 8 {
+      $("javaResult").hide();
+      $("rubyResult").show();
+      $("phpResult").hide();
+      $("csharpResult").hide();
+    }
+    else if (totalVal >= 9 && totalVal <= 14 {
+      $("javaResult").hide();
+      $("rubyResult").hide();
+      $("phpResult").show();
+      $("csharpResult").hide();
+    }
+    else if (totalVal >= 15 && totalVal <= 20 {
+      $("javaResult").hide();
+      $("rubyResult").hide();
+      $("phpResult").hide();
+      $("csharpResult").show();
 
-}
+
+
+
+  })
+  });
+
+});

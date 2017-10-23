5b3d1419e00253886b3f1fc3e5b42b11c59cbb1a
GrantRotzien <grantqrotzien@gmail.com>
2017-10-22 22:03:24 -0700

    fix js code and add html content for form results

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
   var addVal = function (a, b, c, d, e) {
     return a + b + c + d + e;
   };
-  $('form#findtrack').submit(function(event) {
-    event.preventDefault();
+  $("form#tracker").submit(function(event) {
     var a = parseInt($("select#jobtitleInput").val());
     var b = parseInt($("select#mobileorwebInput").val());
     var c = parseInt($("select#startupInput").val());
@@ -11,33 +10,22 @@ $(document).ready(function() {
     var e = parseInt($("select#jokeInput").val());
     var totalVal = addVal(a, b, c, d, e);
     if (totalVal >= 1 && totalVal <= 4) {
-      $("javaResult").show();
-      $("rubyResult").hide();
-      $("phpResult").hide();
-      $("csharpResult").hide();
+      $("#javaResult").fadeIn();
+      $("#tracker").fadeOut();
     }
-    else if (totalVal >= 5 && totalVal <= 8 {
-      $("javaResult").hide();
-      $("rubyResult").show();
-      $("phpResult").hide();
-      $("csharpResult").hide();
+    else if (totalVal >= 5 && totalVal <= 9) {
+      $("#rubyResult").fadeIn();
+      $("#tracker").fadeOut();
     }
-    else if (totalVal >= 9 && totalVal <= 14 {
-      $("javaResult").hide();
-      $("rubyResult").hide();
-      $("phpResult").show();
-      $("csharpResult").hide();
+    else if (totalVal >= 10 && totalVal <= 12) {
+      $("#phpResult").fadeIn();
+      $("#tracker").fadeOut();
     }
-    else if (totalVal >= 15 && totalVal <= 20 {
-      $("javaResult").hide();
-      $("rubyResult").hide();
-      $("phpResult").hide();
-      $("csharpResult").show();
-
-
-
-
-  })
+    else if (totalVal >= 13 && totalVal <= 17) {
+      $("#cSharpResult").fadeIn();
+      $("#tracker").fadeOut();
+    }
+    event.preventDefault();
   });
 
 });
